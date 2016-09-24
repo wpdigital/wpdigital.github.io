42fc54fed8a31001b05ead4ca07589c8aef6a985
wpdigital <bluezoob@gmail.com>
2016-09-24 19:50:36 +0300

    meliora

---
+++ tea/files/весь_чай/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,[k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(R,_(),S,_()),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="весь_чай.html",d="generationDate",e=new Date(1474735751075.19),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="variables",k="OnLoadVariable",l="page",m="packageId",n="ab8d2edf5b5f4cf7955ab0e6be6f4e8e",o="type",p="Axure:Page",q="name",r="Весь Чай",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="a",S="b",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
---
+++ tea/files/главная/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,i,j,[k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(R,_(),S,_()),T,_(),U,_(V,[])),W,_(),X,_());}; 
+var b="url",c="главная.html",d="generationDate",e=new Date(1474735751039.96),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i=true,j="variables",k="OnLoadVariable",l="page",m="packageId",n="9fb265746f7a44518636517d333025e1",o="type",p="Axure:Page",q="name",r="Главная",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="Applied Font",P="borderWidth",Q="adaptiveStyles",R="a",S="b",T="interactionMap",U="diagram",V="objects",W="masters",X="objectPaths";
+return _creator();
+})());
